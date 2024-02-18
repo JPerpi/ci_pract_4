f8ff042204a5680ca2a23e357ea0469cb77ca589
Perpi <jperpi01@gmail.com>
2024-02-18 10:05:59 +0100

    Puntuación 10/10

--- src/factorial.py
+++ src/factorial.py
@@ -1,9 +1,21 @@
+"""Módulo que contiene la funcion factorial"""
 def factorial(n):
+    """
+    Función para cálcular el factorial de un número
+
+    
+    Entrada:
+    - entero: número entero al que cálcular el factorial
+    
+    
+    Salida:
+    - entero: número entero resultado de cálcular el factorial   
+    
+    """
     if not isinstance(n,int):
         raise TypeError("Solo se aceptan enteros")
     if n<0:
         raise ValueError("Los números negativos no son validos")
     if n == 1:
         return 1
-    else:
-        return n*factorial(n-1)
+    return n*factorial(n-1)
